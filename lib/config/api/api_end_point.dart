405987ab11974ff381d51e8ecfe1ec66323c202d
mehdisparktech <mehdisparktech@gmail.com>
2025-09-29 18:38:30 +0600

    lst

--- lib/config/api/api_end_point.dart
+++ lib/config/api/api_end_point.dart
@@ -2,7 +2,6 @@ class ApiEndPoint {
   static const baseUrl = "http://10.10.7.26:5020/api/v1";
   static const imageUrl = "http://103.145.138.74:3000";
   static const socketUrl = "http://103.145.138.74:3001";
-  // http://localhost:3000/api/v1
   static const signUp = "users/sign-up";
   static const verifyEmail = "users/verify-email";
   static const signIn = "users/sign-in";
